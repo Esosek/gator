9014f7b1cc9727ecb1f5b4e064328c3515e535d4
Aleš Zima <zima.ales@seznam.cz>
2025-06-20 15:35:30 +0200

    Added error handling to agg and users commands

--- src/command_handlers/handler_agg.ts
+++ src/command_handlers/handler_agg.ts
@@ -3,12 +3,17 @@ import { fetchFeed } from '../rss_feed'
 const FETCH_URL = 'https://www.wagslane.dev/index.xml'
 
 async function handlerAgg(cmdName: string, ...args: string[]) {
-  const feed = await fetchFeed(FETCH_URL)
-  if (feed) {
-    console.log(feed)
-    feed.channel.item.forEach((i) => {
-      console.log(i)
-    })
+  try {
+    const feed = await fetchFeed(FETCH_URL)
+    if (feed) {
+      console.log(feed)
+      feed.channel.item.forEach((i) => {
+        console.log(i)
+      })
+    }
+  } catch (error) {
+    console.error(error)
+    process.exit(1)
   }
 }
 
--- src/command_handlers/handler_users.ts
+++ src/command_handlers/handler_users.ts
@@ -2,11 +2,19 @@ import { getUsers } from '../lib/db/queries/users'
 import { readConfig } from '../config'
 
 async function handlerUsers(cmdName: string) {
-  const users = await getUsers()
-  const { currentUserName } = await readConfig()
-  for (const user of users) {
-    const isLoggedIn = currentUserName === user.username
-    console.log(`* ${user.username} ${isLoggedIn ? '(current)' : ''}`)
+  try {
+    const users = await getUsers()
+    if (!users.length) {
+      console.log('No users found.')
+      process.exit(1)
+    }
+    const { currentUserName } = await readConfig()
+    for (const user of users) {
+      const isLoggedIn = currentUserName === user.username
+      console.log(`* ${user.username} ${isLoggedIn ? '(current)' : ''}`)
+    }
+  } catch (error) {
+    console.error('Fetching users failed.')
   }
 }
 
